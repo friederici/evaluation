cfff4701978851e2fe5f1b30910eba8d613ecbaa
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-16 12:50:57 +0100

    wary predictor

---
+++ experiment1/wary-predictor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run workflow 1_constant with different number of input files
+
+for i in $(seq 1 10)
+do
+	cd /workflows/data/
+	rm -f *.txt
+	./createData.py custom $i
+	cd /workflows/synthetic/1_const/
+	make clean
+	make dev-run-wary
+	zip /evaluation/experiment1/measurements/WaryPredictor/exp1-$i.zip .nextflow.log TaskScaler* trace*
+done
+
