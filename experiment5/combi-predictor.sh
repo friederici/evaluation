8e6d2a31ee297c37b20462ce96ccbcd041b6d0bd
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-16 16:42:26 +0100

    experiment 5

---
+++ experiment5/combi-predictor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run workflow 3_square with different number of input files
+
+for i in $(seq 1 10)
+do
+	cd /workflows/data/
+	rm -f *.txt
+	./createData.py custom $i
+	cd /workflows/synthetic/3_square/
+	make clean
+	make dev-run-combi
+	zip /evaluation/experiment5/measurements/CombiPredictor/exp5-$i.zip .nextflow.log TaskScaler* trace*
+done
+
