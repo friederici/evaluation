0cd371f5ea9229a0836867bdc6a1569eb0e25372
Florian Friederici <44371773+friederici@users.noreply.github.com>
2023-11-17 17:33:55 +0100

    improved run script

--- experiment1/run.sh
+++ experiment1/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+mkdir -p measurements
+mkdir -p measurements/NonePredictor
+mkdir -p measurements/ConstPredictor
+mkdir -p measurements/LinearPredictor
+mkdir -p measurements/CombiPredictor
+mkdir -p measurements/WaryPredictor
+
 ./none-predictor.sh
 ./const-predictor.sh
 ./linear-predictor.sh
