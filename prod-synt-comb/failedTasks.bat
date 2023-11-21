e26da07c68f4e1bda60f302c6a9b5f94bc091b77
Florian Friederici <friederf@hu-berlin.de>
2023-11-21 15:07:24 +0100

    add failed task summary

---
+++ prod-synt-comb/failedTasks.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd 5_compound
+python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+
+cd ..\6_compound_square
+python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+
+cd ..\7_compound_square_random
+python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+
+cd ..
