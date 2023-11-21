db4a933345d7068f70a693d0d66a24cc2535a101
Florian Friederici <friederf@hu-berlin.de>
2023-11-21 10:00:33 +0100

    combined workflows

---
+++ prod-synt-comb/visuals.bat
@@ -0,0 +1,12 @@
+
+cd 5_compound
+
+python ..\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+
+cd ..\6_compound_square
+
+python ..\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+
+cd ..\7_compound_square_random
+
+python ..\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
