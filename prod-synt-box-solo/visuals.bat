14cc1a5d1d78065223902857eb7874816178134d
Florian Friederici <friederf@hu-berlin.de>
2023-11-21 14:03:40 +0100

    updated boxplots

--- prod-synt-box-solo/visuals.bat
+++ prod-synt-box-solo/visuals.bat
@@ -14,15 +14,3 @@ move *.png ../
 cd ..\4_random
 python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
 move *.png ../
-
-cd ..\5_compound
-python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
-
-cd ..\6_compound_square
-python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
-
-cd ..\7_compound_square_random
-python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
--- prod-synt-solo/scatter.py
+++ prod-synt-solo/scatter.py
@@ -8,7 +8,6 @@ from pathlib import Path
 import numpy as np
 import matplotlib.ticker as ticker
 from matplotlib.axis import Axis
-from matplotlib.dates import DateFormatter
 
 
 def extract_data(data, search):
