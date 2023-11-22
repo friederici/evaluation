b7c9b14554f21acbac9d5450b65bd502be8eaaa7
Florian Friederici <friederf@hu-berlin.de>
2023-11-22 16:11:14 +0100

    second attempt

---
+++ prod-synt-box-solo-2/visuals.bat
@@ -0,0 +1,16 @@
+
+cd 1_const
+python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+move *.png ../
+
+cd ..\2_linear
+python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+move *.png ../
+
+cd ..\3_square
+python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+move *.png ../
+
+cd ..\4_random
+python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+move *.png ../
