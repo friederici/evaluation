f829fc74c481f00bffb1dbb9f1b69358086967be
Florian Friederici <friederf@hu-berlin.de>
2023-11-23 21:00:43 +0100

    visuals

---
+++ prod-rnaseq/failedTasks.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM python summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+
+python summary.py measurements\NonePredictor
---
+++ prod-rnaseq/visuals.bat
@@ -0,0 +1,4 @@
+
+REM python boxplot.py measurements\NonePredictor\ measurements\ConstantPredictor\ measurements\LinearPredictor\ measurements\CombiPredictor\ measurements\WaryPredictor\
+
+python boxplot.py measurements\NonePredictor\
