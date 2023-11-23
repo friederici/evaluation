5517aec2a02cf82d41081bd1dd392fd23d7cd870
Florian Friederici <friederf@hu-berlin.de>
2023-11-23 09:11:55 +0100

    script

--- prod-rnaseq/run.sh
+++ prod-rnaseq/run.sh
@@ -22,26 +22,26 @@ do
 		pwd
 
 		# reset cluster
-		#/workflows/requirements/clear_cluster.sh
-		#/workflows/requirements/apply_cluster.sh
+		/workflows/requirements/clear_cluster.sh
+		/workflows/requirements/apply_cluster.sh
 
 		# clear old outputs
 		cd /nfs/data/output
-		#rm -rf *
+		rm -rf *
 
 		# clear old results
 		cd /nfs/input/pipelines/rnaseq
-		#rm -rf work
-		#rm -rf .nextflow*
-		#rm Task*
+		rm -rf work
+		rm -rf .nextflow*
+		rm Task*
 
 		# run workflow
 		echo nextflow run main.nf -config /workflows/config/rnaseq.config -config $predictor --outdir /nfs/data/output
-		#nextflow run main.nf -config /workflows/config/rnaseq.config -config $predictor --outdir /nfs/data/output
+		nextflow run main.nf -config /workflows/config/rnaseq.config -config $predictor --outdir /nfs/data/output
 
 		# store results
 		echo zip $basedir/measurements/${predictors[$cnf]}/result_rnaseq_$i.zip Task* trace* .nextflow.log*
-		#zip $basedir/measurements/${predictors[$cnf]}/result_rnaseq_$i.zip Task* trace* .nextflow.log*
+		zip $basedir/measurements/${predictors[$cnf]}/result_rnaseq_$i.zip Task* trace* .nextflow.log*
 	done
 
 done
