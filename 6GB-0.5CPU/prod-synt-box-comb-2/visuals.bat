f8ead190cd2299af3d31db6dac113ffcea5b7ec0
Florian Friederici <friederf@hu-berlin.de>
2023-11-29 13:02:05 +0100

    replaced boxplot

--- 6GB-0.5CPU/prod-synt-box-comb-2/visuals.bat
+++ 6GB-0.5CPU/prod-synt-box-comb-2/visuals.bat
@@ -1,15 +1,21 @@
+@echo off
+setlocal 
+  set "YLIM1=180000"
+  set "YLIM2=525000"
 
-cd 5_compound
-python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
+  cd 5_compound
+  python ..\..\..\scripts\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+  move *.pdf ../
 
-cd ..\6_compound_square
-python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
+  cd ..\6_compound_square
+  python ..\..\..\scripts\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+  move *.pdf ../
 
-cd ..\7_compound_square_random
-python ..\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
+  cd ..\7_compound_square_random
+  python ..\..\..\scripts\boxplot.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+  move *.pdf ../
 
-cd ..
+  cd ..
+
+endlocal
 pause
