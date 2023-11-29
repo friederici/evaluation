133c78be76447cfab44a22dd32e6866f15055bc9
Florian Friederici <friederf@hu-berlin.de>
2023-11-29 13:21:18 +0100

    replaced summary

--- 6GB-0.5CPU/prod-synt-solo-2/failedTasks.bat
+++ 6GB-0.5CPU/prod-synt-solo-2/failedTasks.bat
@@ -1,16 +1,16 @@
 @echo off
 
 cd 1_const
-python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+python ..\..\..\scripts\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
 
 cd ..\2_linear
-python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+python ..\..\..\scripts\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
 
 cd ..\3_square
-python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+python ..\..\..\scripts\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
 
 cd ..\4_random
-python ..\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
+python ..\..\..\scripts\summary.py measurements\NonePredictor measurements\ConstantPredictor measurements\LinearPredictor measurements\CombiPredictor measurements\WaryPredictor
 
 cd ..
 pause
