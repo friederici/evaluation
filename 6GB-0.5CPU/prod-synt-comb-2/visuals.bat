5a95572c780e956c639addc3b2a1bb5eebf014d6
Florian Friederici <friederf@hu-berlin.de>
2023-11-29 13:16:56 +0100

    replaced scatter

--- 6GB-0.5CPU/prod-synt-comb-2/visuals.bat
+++ 6GB-0.5CPU/prod-synt-comb-2/visuals.bat
@@ -1,12 +1,19 @@
+@echo off
+setlocal 
+  set "YLIM1=0"
+  set "YLIM2=840000"
 
-cd 5_compound
-python ..\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
+  cd 5_compound
+  python ..\..\..\scripts\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+  move *.pdf ../
 
-cd ..\6_compound_square
-python ..\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
+  cd ..\6_compound_square
+  python ..\..\..\scripts\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+  move *.pdf ../
 
-cd ..\7_compound_square_random
-python ..\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
-move *.png ../
+  cd ..\7_compound_square_random
+  python ..\..\..\scripts\scatter.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+  move *.pdf ../
+
+endlocal
+pause
