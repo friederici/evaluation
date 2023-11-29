c05c619b282cc24b30ecfe64f095ccb7baab2a1b
Florian Friederici <friederf@hu-berlin.de>
2023-11-29 13:25:25 +0100

    replaced comparison

--- 6GB-0.5CPU/prod-synt-box-comb-2/comparison.bat
+++ 6GB-0.5CPU/prod-synt-box-comb-2/comparison.bat
@@ -1,12 +1,12 @@
 @echo off
 cd 5_compound
-python ..\comparison.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+python ..\..\..\scripts\comparison.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
 
 cd ..\6_compound_square
-python ..\comparison.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+python ..\..\..\scripts\comparison.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
 
 cd ..\7_compound_square_random
-python ..\comparison.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
+python ..\..\..\scripts\comparison.py .\measurements\NonePredictor\ .\measurements\ConstantPredictor\ .\measurements\LinearPredictor\ .\measurements\CombiPredictor\ .\measurements\WaryPredictor\
 
 cd ..
 pause
